d62b351c2c90c3f0b89b044a9d21c219233c8651
CodingBash <bbecerr@outlook.com>
2017-02-17 08:48:32 -0600

    Initial commit

---
+++ src/main/java/edu/ilstu/biology/FlyTranscriptionWebappApplication.java
@@ -0,0 +1,12 @@
+package edu.ilstu.biology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyTranscriptionWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyTranscriptionWebappApplication.class, args);
+	}
+}
