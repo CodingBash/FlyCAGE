c35eefb799b52314887ea59060644ffb8060008b
CodingBash <bbecerr@outlook.com>
2017-02-17 09:35:26 -0600

    See #14 - Added mapping for input page

--- src/main/java/edu/ilstu/biology/flytranscriptionwebapp/controller/InputController.java
+++ src/main/java/edu/ilstu/biology/flytranscriptionwebapp/controller/InputController.java
@@ -3,6 +3,7 @@ package edu.ilstu.biology.flytranscriptionwebapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.ilstu.biology.flytranscriptionwebapp.model.GeneForm;
@@ -10,6 +11,16 @@ import edu.ilstu.biology.flytranscriptionwebapp.model.GeneForm;
 @Controller
 public class InputController {
 
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView showInputPage(){
+		ModelAndView mav = new ModelAndView();
+		
+		// TODO: Logic
+		
+		mav.setViewName("input");
+		return mav;
+		
+	}
 	@RequestMapping(value="/", method=RequestMethod.POST)
 	public String processInput(GeneForm form, RedirectAttributes model){
 		// TODO: Logic
