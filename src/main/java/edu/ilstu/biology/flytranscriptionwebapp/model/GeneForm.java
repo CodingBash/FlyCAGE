f989857dbdfc851f0546a36c64baff4d053713bd
CodingBash <bbecerr@outlook.com>
2017-09-17 15:42:17 -0300

    Back-end checkbox form progress -> Able to add some selected values to Map TODO: Need to add thymeleaf binding to rest of checkboxes TODO: Need to work on back-end selective correlation. May either need to map the expression stage to an order of the list (from the xml downloaded from Intermine), or format the xml better. Plan ahead for how to conduct the analysis when data is directly retrieved from Intermine, rather than indirectly from an XML file

--- src/main/java/edu/ilstu/biology/flytranscriptionwebapp/model/GeneForm.java
+++ src/main/java/edu/ilstu/biology/flytranscriptionwebapp/model/GeneForm.java
@@ -1,6 +1,7 @@
 package edu.ilstu.biology.flytranscriptionwebapp.model;
 
 import java.util.List;
+import java.util.Map;
 
 public class GeneForm {
 
@@ -11,6 +12,8 @@ public class GeneForm {
 	private List<String> correlationOptions;
 
 	private String queryComment;
+	
+	private Map<String, Boolean> expressionStages;
 
 	public String getGeneSpecies() {
 		return geneSpecies;
@@ -44,4 +47,12 @@ public class GeneForm {
 		this.queryComment = queryComment;
 	}
 
+	public Map<String, Boolean> getExpressionStages() {
+		return expressionStages;
+	}
+
+	public void setExpressionStages(Map<String, Boolean> expressionStages) {
+		this.expressionStages = expressionStages;
+	}
+	
 }
