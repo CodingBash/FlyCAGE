60ed4e63dd26721ea971d9d0b57bd0c448ce9740
CodingBash <bbecerr@outlook.com>
2017-09-30 21:08:55 -0500

    Fixed expression selection checkbox form. Moved script to fragment. All checkboxes selected by default. More to do:
    Need to be able to select the other expression stages
    Handle invalid expression stage selection (i.e. must select more than two)
    Need to handle correlation errors (this may be already handled by the
    conditional on NANs, although it will simply discard the data)
    Code cleanup, very messy right now

---
+++ src/main/resources/static/script/form-expression-stage-checkbox-selection.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+
+	/*
+	 * Adjust the main-checkbox relative to the input sub-checkbox i.e. if some
+	 * but not all "group"-sub-checkbox is selected, make
+	 * "group"-master-checkbox *indeterminate*
+	 */
+	var adjustMasterCheckboxes = function(subCheckbox) {
+		allClassNames = $(subCheckbox).prop('class').split(" ");
+		checkboxClassName = "";
+		$.each(allClassNames, function(i) {
+			if (allClassNames[i].includes("-sub-checkbox")) {
+				checkboxClassName = allClassNames[i];
+				return false; // break
+			}
+		});
+		groupName = checkboxClassName.substring(0, checkboxClassName
+				.indexOf('-'));
+
+		var oneChecked = false;
+		var allTrue = true;
+		$('.' + groupName + '-sub-checkbox').each(
+				function(index, currentElement) {
+					if (currentElement.checked == true) {
+						oneChecked = true;
+					} else if (currentElement.checked == false) {
+						allTrue = false;
+					}
+				});
+		if (oneChecked && !allTrue) {
+			$('.' + groupName + '-main-checkbox').prop('indeterminate', true);
+		} else if (allTrue) {
+			$('.' + groupName + '-main-checkbox').prop('indeterminate', false);
+			$('.' + groupName + '-main-checkbox').prop('checked', true);
+		} else if (!oneChecked) {
+			$('.' + groupName + '-main-checkbox').prop('indeterminate', false);
+			$('.' + groupName + '-main-checkbox').prop('checked', false);
+		} else {
+			$('.' + groupName + '-main-checkbox').prop('indeterminate', false);
+		}
+	}
+
+	/*
+	 * Change all identical checkboxes relative to the input sub-checkbox
+	 */
+	var changeIdenticalCheckboxes = function(subCheckbox) {
+		wholeId = $(subCheckbox).prop('id');
+		stageId = wholeId.substring(wholeId.indexOf('-'), wholeId.length)
+		identicalCheckboxes = $('[id $= ' + stageId + ']');
+		identicalCheckboxes.prop('checked', subCheckbox.checked);
+
+		identicalCheckboxes.each(function(index, currentCheckbox) {
+			adjustMasterCheckboxes(currentCheckbox);
+		});
+
+	}
+	
+$.each(groups, function(i) {
+	/*
+	 * Event for main-checkbox change
+	 */
+	$('.' + groups[i] + '-main-checkbox').change(function() {
+		checkboxesToChange = $('.' + groups[i] + '-sub-checkbox');
+		checkboxesToChange.prop("checked", this.checked);
+		checkboxesToChange.each(function(index, currentCheckbox) {
+			changeIdenticalCheckboxes(currentCheckbox);
+		});
+	});
+
+	/*
+	 * Event for sub-checkbox change
+	 */
+	$('.' + groups[i] + '-sub-checkbox').change(function() {
+		adjustMasterCheckboxes(this);
+		changeIdenticalCheckboxes(this);
+
+	});
+
+});
